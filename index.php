d2eb0db61602abef2cd621893f1841095e96814a
alexnathanson <alex@alexnathanson.com>
2019-07-17 16:09:10 -0400

    csv

--- index.php
+++ index.php
@@ -2,34 +2,38 @@
 <html>
 
 <head>
-Solar Server!
+<h1>Solar Server!</h1>
 </head>
 
 <body>
 
 <?php
 
-
-// current directory
-echo getcwd() . "\n";
-
-//get the data
-$file = fopen("/home/pi/EPSolar_Tracer/data/tracerData" . date("Y-m-d") . ".csv","r");
-print_r(fgetcsv($file));
-fclose($file);
-
 //variables
 $pvVoltsNow = 3;
 $pvCurrentNow = .33;
 $pvPowerNow = 1;
+$fileName = "/home/pi/EPSolar_Tracer/data/tracerData" . date("Y-m-d") . ".csv";
+
+echo "<h3>Today's date:</h3>" . date("Y-m-d") . "<br>";
+
+echo "<h3>File name:</h3>". $fileName . "<br>";
+
+// current directory
+//echo getcwd() . "\n";
+
+//get the data
+$file = fopen($fileName,"r");
+print_r(fgetcsv($file));
+fclose($file);
+
+
 
 $dataNow = array($pvVoltsNow, $pvCurrentNow, $pvPowerNow);
 
-echo date("Y-m-d") . "<br>";
 
-echo "file name: /home/pi/EPSolar_Tracer/data/tracerData" . date("Y-m-d") . ".csv <br>";
 
-echo "<h3>Present PV Data: </h3>" . json_encode($dataNow);
+echo "<h3>Present PV Data:</h3>" . json_encode($dataNow);
 
 ?>
 
