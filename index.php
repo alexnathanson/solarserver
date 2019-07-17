a5b54354a4349d809ce069fcf10b1d0ffaf85465
alexnathanson <alex@alexnathanson.com>
2019-07-17 16:14:36 -0400

    bigger data

--- index.php
+++ index.php
@@ -10,10 +10,8 @@
 <?php
 
 //variables
-$pvVoltsNow = 3;
-$pvCurrentNow = .33;
-$pvPowerNow = 1;
 $fileName = "/home/pi/EPSolar_Tracer/data/tracerData" . date("Y-m-d") . ".csv";
+$rawDataArray = [];
 
 echo "<h3>Today's date:</h3>" . date("Y-m-d") . "<br>";
 
@@ -22,18 +20,35 @@ echo "<h3>File name:</h3>". $fileName . "<br>";
 // current directory
 //echo getcwd() . "\n";
 
+echo "<h3>PV Data:</h3>";
+
+/*
 //get the data
 $file = fopen($fileName,"r");
 print_r(fgetcsv($file));
 fclose($file);
+*/
 
+// Open the file for reading
+if (($h = fopen("{$fileName}", "r")) !== FALSE) 
+{
+  // Each line in the file is converted into an individual array that we call $data
+  // The items of the array are comma separated
+  while (($data = fgetcsv($h, 1000, ",")) !== FALSE) 
+  {
+    // Each individual array is being pushed into the nested array
+    $rawDataArray[] = $data;		
+  }
 
+  // Close the file
+  fclose($h);
+}
 
-$dataNow = array($pvVoltsNow, $pvCurrentNow, $pvPowerNow);
+// Display the code in a readable format
+echo "<pre>";
+var_dump($rawDataArray);
+echo "</pre>";
 
-
-
-echo "<h3>Present PV Data:</h3>" . json_encode($dataNow);
 
 ?>
 
