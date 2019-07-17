ad3fa466fd7b1ac9c863051733b11c1061a19135
alexnathanson <alex@alexnathanson.com>
2019-07-17 14:09:38 -0400

    php init

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # solarserver
 
 ## setting up a Linux Apache MySQL PHP (LAMP) server on a Raspberry Pi
+No need to setup the wordpress stuff, unless you plan on making a wordpress site.
 https://projects.raspberrypi.org/en/projects/lamp-web-server-with-wordpress/9
---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+Solar Server!
+</head>
+
+<body>
+
+<?php
+
+//variables
+$pvVoltsNow = "";
+$pvCurrentNow = "";
+$pvPowerNow = "";
+
+$dataNow = array($pvVoltsNow, $pvCurrentNow, $pvPowerNow);
+
+echo $dataNow;
+?>
+
+</body>
+</html>
